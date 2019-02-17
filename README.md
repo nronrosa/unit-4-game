028dabfc0995e0192891df6a730df8f63d9e1ae3
Nitza <rondon.nitza@gmail.com>
2019-02-17 17:08:11 -0500

    Crystal Collector files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # unit-4-game
+### What the project does
+A game where a player clicks on crystals that have been assigned random (hidden) numbers. User must click each until the sum of the clicked crystals equal the random number generated to win. If the sum of the crystals is greater than the random number, player loses.
+
+### Why the project is useful
+This project uses jQuery, random number generator and functions.
+
+### How users can get started with the project
+View this video to see a game in action: (https://youtu.be/klN2-ITjRt8)
+
+### How to play
+	* A random number will be displayed to player at start of game.
+	* There are four crystals displayed as buttons.
+	* When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+	* The game will not show the number of the crystal until a player clicks on it.
+	* When a crystal is clicked, the player's score is updated.
+	* WIN: Player wins if their total score matches the random number.
+	* LOSE: Player loses if their total score is greater than the random number.
+	* The game restarts if win or lose. Resetting user's total score to zero and regenerating a new random number to match and new random numbers for crystals.
+	
---
+++ assets/javascript/game.js
@@ -0,0 +1,91 @@
+// set variables for cyrstals to give random number from 1-12
+var ruby = Math.floor(Math.random() * 12) + 1;
+var sapphire = Math.floor(Math.random() * 12) + 1;
+var citrine = Math.floor(Math.random() * 12) + 1;
+var emerald = Math.floor(Math.random() * 12) + 1;
+
+// set var for random start number from 19-120
+var randomNumber = Math.floor(Math.random() * 120 - 19 + 1) + 19;
+
+// set var to total incrementing score
+var totalscore = 0;
+
+// set variable for wins and losses
+var wins = 0;
+var losses = 0;
+
+// display the random number
+// console.log(randomNumber);
+$(".random-number").text(randomNumber);
+
+// set on clicks for crystals and assign values
+$(".ruby").on("click", function () {
+    // debugger;
+    this.value = ruby;
+    console.log("R " + ruby);
+    totalscore = totalscore + ruby;
+    addCrystals();
+    checkScore();
+});
+
+$(".sapphire").on("click", function () {
+    // debugger;
+    this.value = sapphire;
+    console.log("S " + sapphire);
+    totalscore = totalscore + sapphire;
+    addCrystals();
+    checkScore();
+});
+
+$(".citrine").on("click", function () {
+    // debugger;
+    this.value = citrine;
+    console.log("C " +citrine);
+    totalscore = totalscore + citrine;
+    addCrystals();
+    checkScore();
+});
+
+$(".emerald").on("click", function () {
+    // debugger;
+    this.value = emerald;
+    console.log("E " + emerald);
+    totalscore = totalscore + emerald;
+    addCrystals();
+    checkScore();
+});
+
+// check the total score against random number
+// set totalscore to zero and reset randoms
+function checkScore() {
+    // debugger;
+    if (totalscore === randomNumber) {
+        wins++;
+        $(".result-text").text("YOU WIN!");
+        $(".wins").text(wins);
+        resetGame();
+    }
+    else if (totalscore > randomNumber) {
+        losses++;
+        $(".result-text").text("YOU LOST!");
+        $(".losses").text(losses);
+        resetGame();
+    }
+}
+
+//display score 
+function addCrystals() {
+    console.log("totalscore " + totalscore);
+    $(".total-score").text(totalscore);
+}
+
+function resetGame() {
+    totalscore = 0
+    $(".total-score").text("");
+    ruby = Math.floor(Math.random() * 12) + 1;
+    sapphire = Math.floor(Math.random() * 12) + 1;
+    citrine = Math.floor(Math.random() * 12) + 1;
+    emerald = Math.floor(Math.random() * 12) + 1;
+    randomNumber = Math.floor(Math.random() * 120 - 19 + 1) + 19;
+    $(".random-number").text(randomNumber);
+}
